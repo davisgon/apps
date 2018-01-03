2ee68be6f05d3c336f5d84ad8cc648fe248203a1
davisgon <davisgon@gmail.com>
2018-01-03 14:56:33 -0600

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/global", function(req, res) {
-  console.log("iniciooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo");
+  console.log("inicioooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo");
   console.log(req.body.result.parameters);
   var speech =
     req.body.result &&
