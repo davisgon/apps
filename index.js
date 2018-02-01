e500eca43fdbe0796b6dbd4837f8b1892ae3a76c
davisgon <davisgon@gmail.com>
2018-02-01 09:19:33 -0600

    Original1

--- index.js
+++ index.js
@@ -16,15 +16,12 @@ restService.use(bodyParser.json());
 restService.post("/global", function(req, res) {
   console.log("inicio");
   console.log(req.body.result.parameters);
-  console.log(req.body.result.parameters.email);
-
-  
-    var speech =
+  var speech =
     req.body.result &&
     req.body.result.parameters &&
     req.body.result.parameters.echoText
       ? req.body.result.parameters.echoText
-      : "Orden nomero 1234";
+      : "http://portal.gyt.com.gt/gyt-api-crm-web/rs/public/crm/client/insured/carne?P_IDEPOL=1570346&P_IDEASEG=12095773&codProd=GTVG&numPol=197721&hash=44a5d4645a1caafe1f973e7a52482517";
   console.log("Sale a servicio Global");
   return res.json({
     speech: speech,
