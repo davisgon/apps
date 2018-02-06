acbc27b3b24c36d6445b623734a0e66d925fd484
davisgon <davisgon@gmail.com>
2018-02-06 11:40:26 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,52 @@
-"use strict";
-
-const express = require("express");
-const bodyParser = require("body-parser");
-
-const restService = express();
-
-restService.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
-
-restService.use(bodyParser.json());
-
-restService.post("/global", function(req, res) {
-  console.log("inicio");
-  console.log(req.body.result.parameters);
-  var speech =
-    req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.echoText
-      ? req.body.result.parameters.echoText
-      : "http://portal.gyt.com.gt/gyt-api-crm-web/rs/public/crm/client/insured/carne?P_IDEPOL=1570346&P_IDEASEG=12095773&codProd=GTVG&numPol=197721&hash=44a5d4645a1caafe1f973e7a52482517";
-  console.log("Sale a servicio Global");
-  return res.json({
-    speech: speech,
-    displayText: "Carnet Asegurado",
-    source: "webhook-gyt-ws"
-  });
+'use strict';
+var listeningport = process.env.PORT || 3000;
+const express = require('express');
+const bodyParser = require('body-parser');
+var cors = require('cors');
+var request = require('request');
+const app = express();
+var cont = 0;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(listeningport, function () {
+  console.log('BOT listening at ...' + listeningport);
 });
- 
-restService.listen(process.env.PORT || 8000, function() {
-  console.log("Server up and listening");
+// **************************
+// Webhook
+// DGonzalez, RLara
+// 01/02/2018
+// ChatBot Communyty Manager V1
+// **************************
+app.post('/queja', (req, res) => {  
+  cont ++;  
+  console.log(cont);
+  console.log(req.body.result.action);
+  if (req.body.result.action === 'a_fetch_user_details') {
+    if (req.body.result.parameters['email'] != '' ) {
+      cont = 0;
+      var http = require('http');
+      var postData = req.body.result.parameters;
+      console.log(req.body.result.parameters);
+      request.post({
+        uri: 'http://172.20.2.11:8080/gyt-api-ws/rs/public/complaints/request',
+        headers: {
+          'Content-Type': 'application/json;charset=UTF-8'
+        },
+        json: true,
+        body: postData
+      }, function (error, response, body) {
+        console.log(body.message);
+        if (!error && response.statusCode == 200) {
+          return res.json({
+            speech: body.message
+          });
+        } else {
+          return res.json({
+            speech: "En este momento no es posible procesar su solicitud, intenta de nuevo"
+          });
+        }
+      });
+    }
+  }
 });
+// **************************
